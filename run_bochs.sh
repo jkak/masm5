3515c6bdb6f9db9d2182194b703a0e2d65ad5aa6
jungle85gopy <20041020mjsong@163.com>
2015-06-30 18:20:21 +0800

    add bochsrc run_bochs.sh for debug os

---
+++ run_bochs.sh
@@ -0,0 +1,16 @@
+#########################################################################
+# File Name: run_bochs.sh
+# Author: 
+# mail: 
+# Created Time: 2015年06月30日 星期二 17时58分14秒
+#########################################################################
+#!/bin/bash
+
+nasm -f bin design_os.asm -o boot.bin
+echo ""
+
+dd if=boot.bin of=a.img bs=512 count=1 conv=notrunc
+echo ""
+
+bochs 
+
