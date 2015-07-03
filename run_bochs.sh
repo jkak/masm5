287dd225af2f75cb3c8475633e904378e00c87fe
jungle85gopy <20041020mjsong@163.com>
2015-07-03 11:32:40 +0800

    modify bochsrc to add info of disk c modify run_bochs.sh to modify c.img for disk c

--- run_bochs.sh
+++ run_bochs.sh
@@ -6,10 +6,19 @@
 #########################################################################
 #!/bin/bash
 
+# floppy img
 nasm -f bin ./os/os.asm  -o boot.bin
 echo ""
 
 dd if=boot.bin of=a.img bs=512 count=4 conv=notrunc
+echo ""
+
+
+# disk c img
+nasm -f bin ./os/disk_sys_c.asm  -o c.bin
+echo ""
+
+dd if=c.bin of=c.img bs=512 count=4 conv=notrunc
 echo ""
 
 bochs 
