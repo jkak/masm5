c81a5b5f59096ace1b9824cc73290d7eff47e6c2
jungle85gopy <20041020mjsong@163.com>
2015-07-05 22:16:11 +0800

    copy os/os.asm as os/disk_sys_c.asm.     * modify boot label as 80h for disk_sys_c.asm     * modify select_msg and boot_msg     * disk_sys_c.asm same to os.asm except select item 4

--- os/disk_sys_c.asm
+++ os/disk_sys_c.asm
@@ -5,6 +5,8 @@
 
 ;##############################################
 ; Sector 1 (CHS=001)
+; Sector 1 only has func of sectors2mem, 
+;   and then jmp to main_entrance
 
 org	07c00h		; mbr will be load to 0:7c00h
 
@@ -28,6 +30,7 @@ start:
 ;   ah : sub func no, 0 for read, 1 for write.
 ;   dx : logic sector
 ;   es:bx : memory segment and offset.
+
 sectors2mem:
     mov ax, 0h
     mov es, ax
@@ -136,15 +139,14 @@ ah_ret:
 ; func: main_entrance.  do all thing here
 
 main_entrance:
-    mov si, boot_msg
-    call show_init_str  ; ds:si
-
-    call delay
+    call delayL
 
     ; clear screen
     mov cx, 0           ; from row:col = 0:0
     mov dx, 184fh       ;   to row:col = 24:79
     call int10h_clear_screen
+
+    call delayL
 
     ; show select
     mov ax, 105h        ; select 1, 5 lines 
@@ -154,29 +156,32 @@ main_select_lp:
     ; input select
     mov dx, 0h          ; when enter, dx= selected line
     call input_select
+    cmp dx, 0
+    je main_do_item_0   ; only delay
     cmp dx, 1
     je main_do_item_1   ; restart current os
-    nop
     cmp dx, 2
     je main_do_item_2   ; restart from system on c:
-    nop
+    cmp dx, 3
+    je main_do_item_3   ; show clock 
 
     jmp main_select_lp
 
-main_do_item_1:
-    nop
-    call restart_sys
+main_do_item_0:
     call delay
     jmp main_select_lp
+main_do_item_1:
+    call restart_sys
 
 main_do_item_2:
-    nop
     call restart_sys_c
-    call delay
 
+main_do_item_3:
+    call show_clock_ctrl
 
 
 main_select_again:
+    ; jmp for all select je 
     jmp main_select_lp
 
 
@@ -194,8 +199,6 @@ fin:
 ; define data:
 
 select_msg:
-    ;tbl  dw 0, line_4, line_3, line_2, line_1, line_0
-    
     db "Select item to run: (Up|Down)  ", 0
     db "  diskC * reset pc system      ", 0
     db "  diskC * start existed system ", 0
@@ -205,7 +208,7 @@ select_msg:
 
 
 boot_msg:
-    db 0ah, "Hello, disk C OS!", 0
+    db "test info: Hello, disk C OS!", 0
 
 
 
@@ -218,26 +221,47 @@ boot_msg:
 ;##################################
 ; func: show_init_str
 ;   string begin at ds:si, end with NULL
+;   row:col is 10:20
 
 show_init_str:
-    push ax
-    push bx
+    push cx
+    push dx
+    push ds
+    push es
     push si
 
-    mov	ah, 0eh		; show a char
-    mov	bx, 04h		; bh=0 : page no, bl=04h : red color.
-show_init_lp:
-	mov	al, [si]
-    cmp al, 0
-    je show_init_ret
-	int	10h			; show string
-    inc si
-    jmp show_init_lp
+    mov ax, cs          ; src seg
+    mov ds, ax         
+    mov si, boot_msg    ; src offset
+    mov cx, 0b800h      
+    mov es, cx          ; des seg
+    mov cl, 07h
+    mov dh, 10          ; row 10
+    mov dl, 10          ; col 10
 
-show_init_ret:
+    call show_str
+
     pop si
-    pop bx
-    pop ax
+    pop es
+    pop ds
+    pop dx
+    pop cx
+    ret
+
+;#### func end ####
+
+
+
+;###################################
+; func: delayL . wait some seconds longer.
+delayL:
+    push cx
+    mov cx, 10h
+delay_lp:
+    call delay
+    loop delay_lp
+
+    pop cx
     ret
 ;#### func end ####
 
@@ -248,7 +272,7 @@ show_init_ret:
 delay:
     push ax
     push dx
-    mov dx, 3fh
+    mov dx, 08h
     mov ax, 0ffffh
 delay_s1: 
     sub ax, 1
@@ -337,14 +361,12 @@ show_len_end:
 ;   al is num of lines
 
 show_select_item:
-    push ax
-    push bx
     push cx
     push dx
-    push bp
+    push ds
+    push es
     push si
-    push di         ; for backup
-    mov di,ax       ; backup ax
+    push ax
     
     ; clear item lines
     mov cx, 0       ; start line
@@ -353,52 +375,39 @@ show_select_item:
     mov dl, 4fh
     call int10h_clear_screen
 
+    ;#### parameter of show_str
+    mov ax, cs
+    mov ds, ax
     mov si, select_msg  ; select msg addr
+    mov ax, 0b800h
+    mov es, ax
+    mov cl, 07h         ; white color for default
     mov dh, 0h          ; first row at: 0
-    mov dl, 10h         ; first col at: 10h
+    mov dl, 8h          ; first col at: 8h
 
-    mov cx, di
-    mov ch, 0           ; cx is lines
-    mov bx, 0           ; offset for line msg
+    pop ax
 show_select_each_line:
-    push bx             ; line offset
-    push cx             ; lines
-
-    ; check selected line no
-    mov ax, di          ; ah is selected line
-    mov al, 5h
-    sub al, cl          ; al is diff of 5-cx
-    cmp ah, al
+    cmp dh, al          ; al is num of lines
+    jnb show_select_ret
+    cmp dh, ah          ; is selected line?
     je line_equ
-    mov bl, 07h         ; black color
-    jmp line_cmp_end
+    mov cl, 07h         ; white color for default
+    jmp call_show_str
 line_equ:
-    mov bl, 24h         ; red color, green background.
-    call set_cursor
-line_cmp_end:
-    mov di, ax          ; backup again 
-    mov bp, si          ; each line base addr for int 10h
-    call get_str_len    ; length in cx
+    mov cl, 24h         ; red color, green background.
+call_show_str:
+    call show_str
 
-    mov ax, 1300h       ; ah:13h for show string
-    mov bh, 0           ; page 0
-    int	10h		        ; show string
-
-    pop cx
-    pop bx
-
-    inc dh              ; next row to show
-    add si, 20h         ; next line offset
-    loop show_select_each_line
+    add si, 20h
+    inc dh
+    jmp show_select_each_line
 
 show_select_ret:
-    pop di
     pop si
-    pop bp
+    pop es
+    pop ds
     pop dx
     pop cx
-    pop bx
-    pop ax
     ret
 
 ;#### func end ####
@@ -426,6 +435,34 @@ set_cursor:
     ret
 
 ;#### func end ####
+
+
+
+
+;###################################
+; func: read_kb  to read keyboard input
+; return:
+;;;;;;;;;;;;;;   dx: 1 means read success, 0 mean no input
+;   ax: ah: scan code, al ascii. when dx=1
+read_kb:
+   ; mov dx, 0h          ; init dx
+    mov ah, 1           ; test input
+    int 16h             ; scan code in ah, ascii in al
+    je kb_in_ignore     ; ZF=1 means Zero, keyboard buf is blank
+    mov ah, 0           ; read input 
+    int 16h 
+    ;mov dx, 1h
+    jmp kb_in_ret
+
+kb_in_ignore:
+    mov ax, 0h
+kb_in_ret:
+    ret
+
+;#### func end ####
+
+
+
 
 
 
@@ -501,17 +538,14 @@ in_ret:
 ; func: restart_sys to restart system 
 
 restart_sys:
-    push si
-    mov si, boot_msg
-    call show_init_str  ; ds:si
-    call delay
+    call show_init_str 
+    call delayL
 
     push bp
     mov bp, sp
-    mov word [bp+4], 0h      ; modify IP in stack
-    mov word [bp+6], 0ffffh  ; modify CS in stack
+    mov word [bp+2], 0h      ; modify IP in stack
+    mov word [bp+4], 0ffffh  ; modify CS in stack
     pop bp              
-    pop si
     ; now, the top of stack is CS:IP, far return to restart
     retf                
 
@@ -524,6 +558,9 @@ restart_sys:
 ; func: restart_sys_c to restart system on C:
 
 restart_sys_c:
+    call show_init_str
+    call delayL
+
     mov ax, 0h
     mov es, ax
     mov bx, 7c00h   ; es:bx mem addr for copy C: MBR sector
@@ -546,33 +583,211 @@ reset_cs_ip:
 
 
 
+;###################################
+; func: show_clock_ctrl  to control of show clock from cmos
+show_clock_ctrl:
+    push ax
+    push cx
+    push dx
+
+    ; clear show zone
+    mov cx, 1000h           ; start line
+    mov dx, 114fh
+    call int10h_clear_screen
+
+    mov dh, 04h             ; set color
+    mov dl, ch              ; set start line
+clk_ctrl_lp:
+    call show_clock
+    call delay
+    call read_kb
+    ; 0: no input, other: input occur.
+    cmp ax, 0
+    je clk_read_no
+    cmp ah, 01h             ; scan code of ESC
+    je clk_ctrl_ret
+    cmp ah, 3bh             ; scan code of F1
+    je clk_read_f1
+    jmp clk_read_no
+
+clk_read_f1:
+    inc dh                  ; change color
+    cmp dh, 8h              
+    jne clk_read_no
+    mov dh, 1h              ; 111B -> 1000B, roll to 001B
+
+clk_read_no:
+    jmp clk_ctrl_lp
+    
+clk_ctrl_ret:
+    ; clear show zone
+    mov cx, 1000h           ; start line
+    mov dx, 114fh
+    call int10h_clear_screen
+
+    pop dx
+    pop cx
+    pop ax
+    ret
+
+;#### func end ####
+
 
 
 ;###################################
-; func: 
+; func: show_clock to show clock from cmos
+; parameter:
+;   dh: attr of color for clock
+;   dl: start line
+
+show_clock:
+    jmp clk_start
+time_tips   db 'ESC for exit, F1 to change color:', 0
+time_style  db 'yy/mm/dd hh:mm:ss', 0
+time_table  db 9, 8, 7, 4, 2, 0
+;              y  m  d  h  m  second offset of date in cmos
+
+clk_start:
+    push ax
+    push cx
+    push dx
+    push si
+    push di
+    push ds
+    push es
+
+    mov ax, cs
+    mov ds, ax
+    mov di, time_style      ; write index
+    mov si, time_table      ; src nidex of cmos
+    mov cx, 6h
+    call clk_read_cmos
+
+    ; show clock string
+    mov si, time_tips       ; read index of tips
+    mov ax, 0b800h
+    mov es, ax
+    mov cl, dh              ; color
+    mov dh, dl              ; row
+    mov dl, 12              ; col
+    call show_str
+
+    mov si, time_style      ; read index of time
+    inc dh
+    add dl, 4
+    call show_str
+
+    pop es
+    pop ds
+    pop di
+    pop si
+    pop dx
+    pop cx
+    pop ax
+    ret
+
+;#### func end ####
+
+
+
+; #############################################
+; func: read_cmos
+; parameter:
+;   cx: loop times.     ds for segment.
+;   si: src index of cmos
+;   di: write index
+clk_read_cmos:
+    push ax
+    push cx
+    push si
+    push di
+read_each:
+    mov al, [ds:si]     ; from ds
+    out 70h, al 
+    in  al, 71h         ; read port
+    
+    ; compute data
+    push cx
+    mov ah, al
+    mov cl, 4
+    shr ah, cl          ; high
+    and al, 0fh         ;  low
+    add ah, 30h	        ; high to ascii
+    add al, 30h         ;  low to ascii
+    
+    mov [ds:di], ah     ; both ds
+    mov [ds:di+1], al
+    inc si
+    add di, 3           ; except '/' or ':'
+    pop cx
+    loop read_each
+
+    pop di
+    pop si
+    pop cx
+    pop ax
+    ret
 
 ;#### func end ####
 
 
 
 
+; ##############################################
+; sub func: show_str show string end with NULL
+; paramters: 
+;   DS:SI : str_pointer 
+;   ES:   : video segment
+;   CL	  : color, 
+;   DH	  : row(0-24) 0: 0h~9FH, 1: 0A0H~140H
+;   DL    : col(0-79)
+show_str:
+    push ax
+    push bx
+    push cx
+    push si
+    push di
+	
+    ; video pointer
+    mov al, 0A0H	; 160 byte each line.
+    mov bl, dh	    ; row
+    mul bl	        ; MAX result is 4000
+    
+    mov bh, 0	    ; ready for add dl
+    mov bl, dl
+    add ax, bx	    ; add column char offset
+    add ax, bx	    ; add column attr offset
+    mov di, ax	    ; video header pointer
+    mov ah, cl	    ; now ah is color
+    mov cx, 0	    ; CX for jcxz
+    
+show_char:
+    mov cl, [ds:si]
+    jcxz show_done
+    ; display
+    mov al, cl		; char in al, attr in ah
+    mov [es:di], AX	; display char and attr
+    inc si
+    inc di
+    inc di
+    loop show_char
+    
+show_done:
+    pop di
+    pop si
+    pop cx
+    pop bx
+    pop ax
+    ret
+    
+;#### func end ####
+
+; #############################################
 
 
 
 
 
-; 200h sector1
-;    db "12345678"
-;    times 1022-($-$$) db 0   ; fill with 0 to logic sector 1
-;    db "ok;"
 
-; 400h sector2
-    db "ABCDEFGH"
-    times 1534-($-$$) db 0   ; fill with 0 to logic sector 2
-    db "ok"
 
-; 600h sector3
-    db "abcdefgh"
-    times 2046-($-$$) db 0   ; fill with 0 to logic sector 3
-    db "ok"
 
